8cfc8e86d9d8a4ce401c5e14c55aa1105f7a43a5
glebgav <glebgav@gmail.com>
2021-02-19 17:37:38 +0200

    security for sign up an sign in

--- src/main/java/com/sadna/app/ws/MySCRUM/MyScrumApplication.java
+++ src/main/java/com/sadna/app/ws/MySCRUM/MyScrumApplication.java
@@ -1,5 +1,6 @@
 package com.sadna.app.ws.MySCRUM;
 
+import com.sadna.app.ws.MySCRUM.security.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
--- src/main/java/com/sadna/app/ws/MySCRUM/security/SecurityConstants.java
+++ src/main/java/com/sadna/app/ws/MySCRUM/security/SecurityConstants.java
@@ -9,7 +9,7 @@ public class SecurityConstants {
     public static final String SIGN_UP_URL = "/users";
 
     public static String getTokenSecret(){
-        AppProperties appProperties = (AppProperties) SpringApplicationContext.getBean("AppProperties");
+        AppProperties appProperties = (AppProperties) SpringApplicationContext.getBean("appProperties");
         return appProperties.getTokenSecret();
     }
 
